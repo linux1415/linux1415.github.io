e50b2a91e06f92d6abf186f7a707c879821ba579
linux1415 <42008960+linux1415@users.noreply.github.com>
2019-06-22 17:59:25 -0500

    Delete go.py

--- go.py
+++
@@ -1,6 +0,0 @@
-#must be run from github folder; username: linux1415
-import os
-os.system('git config --global user.email jfsportsaddict@gmail.com')
-os.system('git add --all')
-os.system('git commit -m "Initial commit"')
-os.system('git push -u origin master')
