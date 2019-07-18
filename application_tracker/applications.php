c3303f961755fe39ef190952a83a8d496dcf3057
linux1415 <42008960+linux1415@users.noreply.github.com>
2019-07-18 14:13:26 -0700

    Add files via upload

--- application_tracker/applications.php
+++ application_tracker/applications.php
@@ -50,6 +50,24 @@ include 'verify.php';
 		var id = a;
 		window.location.href = "delete.php?num=" + id;
 	}
+	
+	function month() {
+		var select = document.getElementById("month");
+		var month = select.options[select.selectedIndex].value;
+		return month;
+	}
+
+	function year() {
+		var select = document.getElementById("year");
+		var year = select.options[select.selectedIndex].value;
+		return year;
+	}
+
+	function build_url() {
+		var chosen_month = month();
+		var chosen_year = year();
+		window.location.href = "applications.php?year=" + chosen_year + "&month=" + chosen_month;
+	}
 	</script>
 	<title>Application Tracker</title>
   </head>
@@ -58,7 +76,45 @@ include 'verify.php';
 		<div class="jumbotron">
 			<h1 class="display-4">Job Applications</h1>
 			<hr class="my-4">
-			<a class="btn btn-primary btn-lg" href="home.php" style="width:7em;" role="button">Home</a>&nbsp;&nbsp;<a class="btn btn-primary btn-lg" href="new.php" style="width:7em;" role="button">New Entry</a>&nbsp;&nbsp;<a class="btn btn-primary btn-lg" href="logout.php" style="width:7em;" role="button">Logout</a>
+			<a class="btn btn-primary btn-lg mb-3" href="home.php" style="width:7em;" role="button">Home</a>&nbsp;&nbsp;<a class="btn btn-primary btn-lg mb-3" href="new.php" style="width:7em;" role="button">New Entry</a>&nbsp;&nbsp;<a class="btn btn-primary btn-lg mb-3" href="logout.php" style="width:7em;" role="button">Logout</a>
+		</div>
+		<div class="row no-gutters">
+		<div class="col mb-3">
+		<select class="form-control" id="month" onchange="build_url()">
+		<option value="default">Month</option>
+		  <option value="1">January</option>
+		  <option value="2">February</option>
+		  <option value="3">March</option>
+		  <option value="4">April</option>
+		  <option value="5">May</option>
+		  <option value="6">June</option>
+		  <option value="7">July</option>
+		  <option value="8">August</option>
+		  <option value="9">September</option>
+		  <option value="10">October</option>
+		  <option value="11">November</option>
+		  <option value="12">December</option>
+		</select>
+		</div>
+		<div class="col mb-3">
+		<select class="form-control" id="year" onchange="build_url()">
+		<option value="default">Year</option>
+
+		<? 
+		include 'database_connect.php';
+		$user = $_SESSION['id_app_tracker'];
+		$query = "SELECT YEAR(date) as year FROM applications where user = '$user' group by year;";
+			
+
+		//running previously created query and creating table of statistics
+		$array = mysqli_query($conn, $query);
+		while ($row = mysqli_fetch_array($array, MYSQLI_ASSOC)){
+			echo '<option value=\'' . $row['year'] . '\'>' . $row['year'] . '</option>';
+		}
+		?>
+
+		</select>
+		</div>
 		</div>
 		<input class="form-control" oninput="w3.filterHTML('#my_table', '.item', this.value)" placeholder="Filter" style="text-align:center;">
 		<table class="table"  id="my_table" class="tablesorter">
@@ -75,9 +131,37 @@ include 'verify.php';
 			</thead>
 			<tbody>
 		<?
-		include 'database_connect.php';
-		$user = $_SESSION['id_app_tracker'];
-		$query = "select * from applications where user = '$user' order by date desc;";
+		$query = "select * from applications where user = '$user'";
+		
+		$current_year = date('Y');
+
+		if(isset($_GET['month'])){ 
+			$month = $_GET['month'];
+			if ($month != 'default') {
+				if(isset($_GET['year'])){ 
+					$query = $query . " and MONTH(date) = '$month'";
+				}
+				else {
+					$query = $query . " and MONTH(date) = '$month' and YEAR(date) = '$current_year'";
+				}
+				echo "<script>";
+				echo "document.getElementById(\"month\").value=\"" . $month . "\";";
+				echo "</script>";
+			}
+		}
+
+		if(isset($_GET['year'])){ 
+			$year = $_GET['year'];
+			if ($year != 'default') {
+				$query = $query . " and YEAR(date) = '$year'";
+				echo "<script>";
+				echo "document.getElementById(\"year\").value=" . $year . ";";
+				echo "</script>";
+			}
+		}
+		
+		$query = $query . " order by date desc;";
+
 		$array = mysqli_query($conn, $query);
 
 		while ($row = mysqli_fetch_array($array, MYSQLI_ASSOC)){
@@ -90,8 +174,8 @@ include 'verify.php';
 			$notes = $row['notes'];
 			$id = $row['id'];
 			echo "<tr class=\"item\">";
-			echo "<td>$comp</td><td>$title</td><td>$screen</td><td>$interview</td><td>$status</td><td>$date</td><td style='text-align:center;'><button type='button' style='width:4em;' onclick='add(\"$id\")' class='btn btn-primary btn-sm'>Edit</button>&nbsp;<button type='button' onclick='del(\"$id\")' class='btn btn-danger btn-sm'>Delete</button>";
-				echo "&nbsp;<button type='button' style = 'width:5.5em' class='btn btn-success btn-sm' data-toggle='modal' data-target='#exampleModalCenter_$id'>Notes";
+			echo "<td>$comp</td><td>$title</td><td>$screen</td><td>$interview</td><td>$status</td><td>$date</td><td style='text-align:center;'><button type='button' style='width:4em;' onclick='add(\"$id\")' class='btn btn-primary btn-sm mb-3'>Edit</button>&nbsp;<button type='button' onclick='del(\"$id\")' class='btn btn-danger btn-sm mb-3'>Delete</button>";
+				echo "&nbsp;<button type='button' style = 'width:5.5em' class='btn btn-success btn-sm mb-3' data-toggle='modal' data-target='#exampleModalCenter_$id'>Notes";
 				if (strlen($notes) > 1) {
 					echo "&nbsp;<span class='badge badge-light'>&#10003;</span>";
 				}
